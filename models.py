c676834950c9b3b4194528da6fb688b7b9feeeef
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-24 14:35:56 +0100

    Create "Distro" model

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Distro(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    name = db.Column(db.String(80), nullable=False, unique=True)  # Distribution name (must be unique)
+    version = db.Column(db.String(20), nullable=True)  # Optional version
+    release_date = db.Column(db.Date, nullable=True)  # Release date of the distribution
+    architecture = db.Column(db.String(20), nullable=False, default="x86_64")  # CPU architecture (default: x86_64)
+    website = db.Column(db.String(200), nullable=True)  # Official website of the distribution
+
+    def __repr__(self):
+        return f"<Distro {self.name} {self.version}>"
