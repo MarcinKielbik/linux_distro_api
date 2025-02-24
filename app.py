07df8abc0d05651af17c7732cfe39c4a49a6701d
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-24 15:01:04 +0100

    Create 'GET' methods, first migration

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
+from models import db, Distro
+from config import Config
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
-api = Api(app)
+app.config.from_object(Config)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+migrate = Migrate(app, db)
+
+
+@app.route('/distros', methods=['GET'])
+def get_all_distros():
+    distros = Distro.query.all()
+    return jsonify([distro.to_dict() for distro in distros])
+
+
+@app.route('/distro/<int:distro_id>', methods=['GET'])
+def get_distro(distro_id):
+    distro = Distro.query.get(distro_id)
+    if distro:
+        return jsonify(distro.to_dict())
+    return jsonify({"message": "User not found"}), 404
+
 
 @app.route('/')
 def index():
